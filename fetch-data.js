bbb52e0768757317e3f3deaf58a518931e589a24
yahya <yahyands@gmail.com>
2024-10-10 17:28:04 +0100

    Initial commit of js project

---
+++ fetch-data.js
@@ -0,0 +1,27 @@
+async function fetchUserData() {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+
+  try {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    const userList = document.createElement("ul");
+
+    dataContainer.innerHTML = "";
+    console.log(users);
+
+    users.forEach((user) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = user.name;
+      userList.appendChild(listItem);
+    });
+
+    dataContainer.appendChild(userList);
+    console.log(userList);
+  } catch (error) {
+    dataContainer.innerHTML = "";
+    dataContainer.textContent = "Failed to load user data.";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchUserData);
